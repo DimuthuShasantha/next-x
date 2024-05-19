ae5a166cc8a9bea93a9f681d57f5342494132d4a
DimuthuShasantha <mdimuthusw@gmail.com>
2024-05-19 10:19:30 +0530

    Modify the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+import { FaXTwitter } from "react-icons/fa6";
+import { HiHome } from "react-icons/hi";
+
 export default function Sidebar() {
   return (
-    <div>
-      Sidebar
+    <div className="flex flex-col gap-3 p-3">
+      <Link href="/">
+        <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200" />
+      </Link>
+      <Link
+        href="/"
+        className="flex items-center p-3 hover:bg-gray-100 rounded-full transistion-all gap-2 w-fit duration-200"
+      >
+        <HiHome className="w-7 h-7" />
+        <span className="hidden font-bold xl:inline">Home</span>
+      </Link>
+      <button className="bg-blue-400 text-white rounded-full hover:brightness-95 shadow-md transistion-all duration-200 w-48 h-9">
+        Sign In
+      </button>
     </div>
-  )
+  );
 }
